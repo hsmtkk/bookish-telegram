7ecd5d64cdcc24e955defde9dda39d533e7e9d9c
hsmtkk <hsmtkk@gmail.com>
2022-11-29 15:30:21 +0900

    add web server  code

--- main.ts
+++ main.ts
@@ -2,12 +2,69 @@
 // SPDX-License-Identifier: MPL-2.0
 import { Construct } from "constructs";
 import { App, TerraformStack, CloudBackend, NamedCloudWorkspace } from "cdktf";
+import * as google from '@cdktf/provider-google';
+
+const project = 'bookish-telegram';
+const region = 'asia-northeast1';
+const repository = 'bookish-telegram';
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // define resources here
+    new google.provider.GoogleProvider(this, 'google', {
+      project,
+      region,
+    });
+
+    const cloud_run_service_account = new google.serviceAccount.ServiceAccount(this, 'cloud_run_service_account', {
+      accountId: 'cloud-run-service-account',
+      displayName: 'service account for Cloud Run',
+    });
+
+    const example_service = new google.cloudRunService.CloudRunService(this, 'cloud_run_service', {
+      location: region,
+      name: 'example',
+      template: {
+        spec: {
+          containers: [{
+            image: 'us-docker.pkg.dev/cloudrun/container/hello',
+          }],
+          serviceAccountName: cloud_run_service_account.email,
+        },
+      },
+    });
+
+    const cloud_run_no_auth = new google.dataGoogleIamPolicy.DataGoogleIamPolicy(this, 'cloud_run_no_auth', {
+      binding: [{
+        members: ['allUsers'],
+        role: 'roles/run.invoker',
+      }],
+    });
+
+    new google.cloudRunServiceIamPolicy.CloudRunServiceIamPolicy(this, 'example_service_no_auth', {
+      location: region,
+      service: example_service.name,
+      policyData: cloud_run_no_auth.policyData,
+    });
+
+    new google.cloudbuildTrigger.CloudbuildTrigger(this, 'cloud_build_trigger', {
+      filename: 'cloudbuild.yaml',
+      github: {
+        owner: 'hsmtkk',
+        name: repository,
+        push: {
+          branch: 'main',
+        },
+      },
+    });
+
+    new google.artifactRegistryRepository.ArtifactRegistryRepository(this, 'artifact_registry', {
+      format: 'docker',
+      location: region,
+      repositoryId: 'registry',
+    });
+
   }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Result};
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    let port = get_port().unwrap();
+    let addr: std::net::SocketAddr = format!("0.0.0.0:{}", port).parse().unwrap();
+
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+fn get_port() -> Result<u32> {
+    let port_str = std::env::var("PORT")?;
+    let port: u32 = port_str.parse::<u32>()?;
+    Ok(port)
 }
